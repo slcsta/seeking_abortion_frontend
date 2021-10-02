d039540b077696393ac1811df8991462369349f0
Sarah <sarahlchavez@gmail.com>
2021-10-02 05:35:21 -0700

    added fetch request to clinics index backend in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('hello')
+
+// good practice to wrap our fetch request in a function
+// fetch request for clinics
+function getClinics(){
+    fetch('http://localhost:3000/items')
+    .then(response => response.json())
+    .then(data = renderClinics(data))
+}
+
+// and then because we wrapped fetch in a function we need to call it down at the bottom
+getClinics()
