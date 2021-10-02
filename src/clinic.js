32839140fcd996520919758afcdfa7cb8a657a4d
Sarah <sarahlchavez@gmail.com>
2021-10-02 05:29:40 -0700

    added pseudocode to clinic.js

---
+++ src/clinic.js
@@ -0,0 +1,4 @@
+// Needs to remember all the clinics
+// Responsible for setting the properties of each item
+// Responsible for the HTML we want to add to the DOM for each item
+// Attach any event linsteners particular to each item.
