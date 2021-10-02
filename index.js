fcc7f5f9ab75a228e6978f5811e4b460b8c257ed
Sarah <sarahlchavez@gmail.com>
2021-10-02 12:13:39 -0700

    updated fetch request in index.js to ensure it's working

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ console.log('hello')
 
 // good practice to wrap our fetch request in a function
 // fetch request for clinics
-function getClinics(){
-    fetch('http://localhost:3000/items')
+// when info gets sent over the internet it gets turned into a string so we have to jsonify again
+//function getClinics(){
+    fetch('http://localhost:3000/clinics')
     .then(response => response.json())
-    .then(data = renderClinics(data))
-}
+    .then(data => console.log(data))
 
-function renderItems(data){
-    const clinics = data["data"];
-    clinics.forEach((clinic) =>{
-
-    })
-}
+// function renderItems(data){
+//     const clinics = data["data"];
+//     // clinics.forEach((clinic) => {
+//     }
+//}
 
 // and then because we wrapped fetch in a function we need to call it down at the bottom
-getClinics()
+// getClinics()
