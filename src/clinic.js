3f67b43f8000781e249f9f12f09047ca062a816c
Sarah <sarahlchavez@gmail.com>
2021-10-05 11:28:34 -0700

    modified render HTML function to work with clinic as a class

--- src/clinic.js
+++ src/clinic.js
@@ -11,14 +11,33 @@ class Clinic {
         this.address = address 
         this.city = city
         this.zip_code = zip_code
-        this.state_id = state_id
+        //this.state_id = state_id
         this.phone_number = phone_number
         this.id = id  
-        this.state = state
+        //this.state = state
+        this.element = document.createElement('li');
         all.push(this)
-        //this.element = document.createElement('li');
+        
         //this.element.dataset['id'] = id;
         //this.element.id = `clinic-${id}`;
+    }
 
+    render() {
+        //const li = document.createElement('li');
+        this.element.innerHTML = `
+        <div>
+        <span class="name">${this.name}</span>
+        <span class="address">${this.address}</span>
+        <span class="city">${this.city}</span>
+        <span class="zip_code">${this.zip_code}</span>
+        <span class="phone_number">${thisphone_number}</span>
+        </div>
+        <button class="edit" data-id= "${this.id}">Edit</button>
+        <button class="delete" data-id= "${this.id}">Delete</button>
+        `
+        ul.appendChild(li)
+        form.reset() //reset form to be cleared after something is entered
+        // going to put event listener on the parent element li instead of on the button child element - b/c it's simpler
+        li.addEventListener('click', handleClick)
     }
 }
